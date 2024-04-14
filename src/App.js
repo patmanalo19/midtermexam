5c9b8080a9f405d13ae35eba3b152ccdbf452de5
patmanalo19 <pviena27@gmail.com>
2024-04-14 13:14:00 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Viena(display) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <p className="fullname">{display.fullname}</p>
     </div>
   );
-}
+};
 
-export default App;
+function Viena2() {
+  return (
+    <div class="container">
+        <div class="quadrant1">
+            <div class="one">
+                <p>1</p>
+            </div>
+            <div class="two">
+                <p>2</p>
+            </div>
+        </div>
+
+        <div class="quadrant2">
+            <div class="three">
+                <p>3</p>
+            </div>
+            <div class="four">
+                <p>4</p>
+            </div>
+        </div>
+
+        <div class="quadrant3">
+            <div class="five">
+                <p>5</p>
+            </div>
+            <div class="six">
+                <p>6</p>
+            </div>
+        </div>
+
+        <div class="quadrant4">
+            <div class="seven">
+                <p>7</p>
+            </div>
+            <div class="eight">
+                <p>8</p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div>
+      <Viena fullname="Patrick Viena" />
+      <Viena2 />
+    </div>
+  );
+};
